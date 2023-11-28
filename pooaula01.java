5505c70102d409fc49a6a44c718f847a002ebf25
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-11-27 21:49:43 -0300

    Add files via upload

---
+++ pooaula01.java
@@ -0,0 +1,17 @@
+package pooaula01;
+
+    public class pooaula01 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bic";
+        c1.cor = "Preto";
+        c1.ponta = 0.5 ;
+        c1.tampar();
+        c1.status();
+        
+        c1.rabiscar();
+        
+        
+    }
+}
